cc0f2af50e13a0ada3b28168959f7b416756da80
Alexahash <jackasus29072022@gmail.com>
2024-01-12 01:31:51 +0530

    ok

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -5,7 +5,7 @@ import Item from '../Items/items';
 const Popular = () => {
     const [popularProducts , setPopularProducts] = useState([]);
     useEffect(()=>{
-        fetch('http://localhost:4000/popularinwomen')
+        fetch('https://backend-lcua.onrender.com/popularinwomen')
         .then((response)=>response.json())
         .then((data)=>setPopularProducts(data));
       },[])
